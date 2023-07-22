e5db56c7420198c68840f5567c724fcf7ffa3e84
Maleina Bidek <maleina@ucla.edu>
2023-07-22 15:41:35 -0400

    feat: Camera and photo library picking

--- MemeMeV1.0/ViewController.swift
+++ MemeMeV1.0/ViewController.swift
@@ -9,14 +9,24 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: Outlets
+    
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     // MARK: Actions
     
-    // This code was borrowed from Build V1.0 of the MemeMe App lesson
+    // This code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
     @IBAction func pickAnImage(_ sender: Any) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    @IBAction func takeAPicture(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
         present(pickerController, animated: true, completion: nil)
     }
     
@@ -27,7 +37,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
     // MARK: Delegate methods
+    
+    // This code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                     imagePickerView.image = image
