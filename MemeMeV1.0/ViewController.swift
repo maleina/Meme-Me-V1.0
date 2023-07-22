b707be8fc1ac3a74ad71d1882d6aea0396976f5e
Maleina Bidek <maleina@ucla.edu>
2023-07-22 17:39:20 -0400

    feat: Add meme text logic

--- MemeMeV1.0/ViewController.swift
+++ MemeMeV1.0/ViewController.swift
@@ -14,27 +14,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imagePickerView: UIImageView!
     
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
+    // MARK: Text Field Delegate
+    
+    let textFieldDelegate = TextFieldDelegate()
+    
+    
+    //MARK: Constants
+    
+    // Meme text attribures dictionary
+    // Some of this code is borrowed from Build V1.0 of the MemeMe App lesson
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -5.0
+    ]
+    
+    
     // MARK: Actions
     
-    // This code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
+    // The code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
     @IBAction func pickAnImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
-        pickerController.sourceType = .photoLibrary
-        present(pickerController, animated: true, completion: nil)
+        pickMemeImage(sourceType: .photoLibrary)
     }
     @IBAction func takeAPicture(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
-        pickerController.sourceType = .camera
-        present(pickerController, animated: true, completion: nil)
+        pickMemeImage(sourceType: .camera)
     }
     
     // MARK: Life Cycle Meethods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set default text and properties for meme text
+        styleMemeText(textField: topTextField, defaultText: "TOP")
+        styleMemeText(textField: bottomTextField, defaultText: "BOTTOM")
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,7 +64,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // MARK: Delegate methods
     
-    // This code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
+    // The code in these methids was borrowed from Build V1.0 of the MemeMe App lesson
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                     imagePickerView.image = image
@@ -56,5 +77,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
+    // MARK: Helper Methods
+    
+    func styleMemeText(textField: UITextField, defaultText: String) {
+        textField.delegate = textFieldDelegate
+        textField.text = defaultText
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+    }
+    
+    func pickMemeImage (sourceType: UIImagePickerController.SourceType) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = sourceType
+        present(pickerController, animated: true, completion: nil)
+    }
 }
 
