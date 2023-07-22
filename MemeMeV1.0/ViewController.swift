6c740644e3a75dbe1a7347024f28409ec76037c2
Maleina Bidek <maleina@ucla.edu>
2023-07-22 09:29:41 -0400

    Initial Commit

---
+++ MemeMeV1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMeV1.0
+//
+//  Created by Maleina Bidek on 7/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
